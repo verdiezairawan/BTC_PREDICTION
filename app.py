17b957aff7c6beaaeeed229da5aeefe694abdb5a
Verdi Eza Irawan <verdiezairawan@gmail.com>
2025-07-29 19:17:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from keras.models import load_model
+from tensorflow.keras.models import load_model # <-- Perubahan di sini
 import joblib
 import plotly.graph_objects as go
 
